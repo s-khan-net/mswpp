e767ec7c49de10ac463514ff9095b69a316d550a
Saudahmed Khan <saudahmed.khan@SPI.COM>
2019-09-10 16:02:59 +0530

    remove default links on submenu

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@
             //$(this).parent().siblings().removeClass('open');
             $(this).addClass('open');
         });
+        $('.dropdownagain').on('click', function (event) {
+            //event.preventDefault();
+            event.stopPropagation();
+            //alert();
+        });
         $('.dropdownagain').on('mouseout', function (event) {
             event.preventDefault();
             event.stopPropagation();
@@ -22,10 +27,10 @@
         $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
-            //$(this).parent().siblings().removeClass('open');
-            //$(this).parent().toggleClass('open');
+            $(this).parent().siblings().removeClass('open');
+            $(this).parent().toggleClass('open');
             //alert();
-            window.location.href = $(this).attr('href');
+            //window.location.href = $(this).attr('href');
         });
     });
 })(jQuery);
