63dafca211db415a32a295ef486609a68810168d
saudkhan <saudkhan03@outlook.com>
2019-09-10 10:31:19 +0530

    initial

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+(function ($) {
+    $(document).ready(function () {
+        $('ul.dropdown-menu [data-toggle=dropdown]').on('mouseover', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).parent().siblings().removeClass('open');
+            $(this).parent().toggleClass('open');
+        });
+        $('.dropdownagain').on('mouseover', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            //$(this).parent().siblings().removeClass('open');
+            $(this).addClass('open');
+        });
+        $('.dropdownagain').on('mouseout', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            //$(this).parent().siblings().removeClass('open');
+            $(this).removeClass('open');
+        });
+        $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            //$(this).parent().siblings().removeClass('open');
+            //$(this).parent().toggleClass('open');
+            //alert();
+            window.location.href = $(this).attr('href');
+        });
+    });
+})(jQuery);
